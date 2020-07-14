86867951a3c30fa0d6b3c8611e4afd5c31b99889
chisakondo <shelif3239saku@i.softbank.jp>
2020-07-14 19:20:44 +0900

    first_commit

---
+++ pages/business.vue
@@ -0,0 +1,377 @@
+<template>
+  <html>
+    <body id="top">
+      <div class="wrap">
+
+        <!-- Page Header -->
+        <header class="h-header">
+          <h1 class="h-logo">
+            <a href="https://www.jenerate.co.jp">
+              <img class="h-logo__img1" src="logo1.png">
+              <img class="h-logo__img2" src="logo2.png">
+            </a>
+          </h1>
+          <ul class="h-gNav__nav2--en">
+            <li>
+              <a href="https://www.jenerate.co.jp/">JP</a>
+            </li>
+            <li>
+              <a class="current">EN</a>
+            </li>
+          </ul>
+          <nav class="h-gNav">
+            <ul class="h-gNav__nav1">
+              <li id="menu-item-50" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-11 current_page_item menu-item-50">
+                <router-link to="">私たちについて</router-link>
+              </li>
+              <li id="menu-item-47" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47">
+                <router-link to="/business">事業案内</router-link>
+              </li>
+              <li id="menu-item-48" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-48">
+                <a href="https://www.jenerate.co.jp/companyprofile/">会社情報</a>
+              </li>
+              <li id="menu-item-526" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-526">
+                <a href="https://www.jenerate.co.jp/recruit/">採用情報</a>
+              </li>
+              <li id="menu-item-527" class="is-hide0805 menu-item menu-item-type-post_type_archive menu-item-object-report menu-item-527">
+                <a href="https://www.jenerate.co.jp/report/">レポート</a>
+              </li>
+            </ul>
+            <ul class="h-gNav__nav2">
+              <li>
+                <a href="https://www.jenerate.co.jp/">JP</a>
+              </li>
+              <li>
+                <a href="https://www.jenerate.co.jp/english">EN</a>
+              </li>
+            </ul>
+            <a class="c-btn4" href="https://www.jenerate.co.jp/contactus/">
+              <span>CONTACT</span>
+            </a>
+            <div class="h-menu">
+              <img class="h-menu-df" src="menu1.png" alt>
+              <img class="h-menu-bk" src="menu1-bk.png" alt>
+            </div>
+          </nav>
+          <nav class="c-nav4">
+            <div class="c-nav4__inner">
+              <div class="c-nav4__img c-bg1" style="background-image:url('https://www.jenerate.co.jp/content/wp-content/themes/jp/assets/images/top/img3.jpg');"></div>
+              <div class="c-nav4__box">
+                <div>
+                  <div class="c-nav4__logo">
+                    <img class="h-menu-df" src="txt1.png">
+                  </div>
+                  <div class="c-nav4__main">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <aside class="aside">
+          <div class="container container-1">
+            <div class="c-mv1__ttlBlock">
+              <h2 class="as-2-1">
+                事業案内
+              </h2>
+              <p>
+                SERVICE
+              </p>
+            </div>
+            <div class="c-mv1__imgBlock c-bg1" style="background-image:url('statuc/img-5.jpg');">
+            </div>
+          </div>
+        </aside>
+
+        <!-- Main Content -->
+        <main>
+        <section class="u-pt6emu-pt2em--sp">
+          <h3 class="c-ttl4-2">
+            Zero to
+            <strong>One</strong>
+          </h3>
+          <div class="c-lead1 u-mt1em u-taCenter u-mb50px">
+            <p>
+              <span class="js-eff1">
+                プロフェッショナルサービスや事業投資をコアに、新規事業創造を実現します
+              </span>
+            </p>
+          </div>
+          <div class="l-inner1">
+            <ul class="u-grid">
+              <li class="u-col4 u-col12--sp">
+                <a class="c-card1eff-txt1" href="">
+                  <p class="c-card1__img c-bg1 js-eff1" style="backgrond-image:url(https://www.jenerate.co.jp/content/wp-content/themes/jp/assets/images/common/img1.jpg);">
+                  </p>
+                  <div class="c-card1_main">
+                    <div class="c-card__leadeff-txt1__item">
+                      <p>
+                        <span>コーポレート・アントレプレナーシップ</span>
+                      </p>
+                      <p>
+                        <span>の浸透にチャレンジしています。</span>
+                      </p>
+                    </div>
+                    <p class="c-card1__ttl c-border1">
+                      新規事業コンサルティング
+                    </p>
+                    <div class="c-card__txt">
+              豊富な経験とリソースを武器に大企業が仕掛ける新規事業を支援します。企画から実行までを一気通貫でサポートすることで新規事業の成功確率を高め、コーポレートアントレプレナーシップの浸透を目指します。
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <li class="u-col4 u-col12--sp">
+                <a class="c-card1 eff-txt1" href="">
+                  <p class="c-card1__img c-bg1 js-eff1" style="background-image:url('https://www.jenerate.co.jp/content/wp-content/themes/jp/assets/images/common/img2.jpg');">
+                  </p>
+                  <div class="c-card1__lead eff-txt1__item">
+                    <p>
+                      <span>グローバルスタートアップの日本参入</span>
+                    </p>
+                    <p>
+                      <span>をプロデュースします。</span>
+                    </p>
+                  </div>
+                  <p class="c-card1__ttl c-border1">
+                    ジャパンエントリー
+                  </p>
+                  <div class="c-card1__txt">
+              革新的なビジネスアイデアやテクノロジーで世界を席巻する破壊的なスタートアップの日本市場参入を支援します。
+                  </div>
+                </a>
+              </li>
+              <li class="u-col4 u-col12--sp">
+                <a class="c-card1 eff-txt1" href="">
+                  <p class="c-card1__img c-bg1 js-eff1" style="background-image:url("/static/img3.jpg");">
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="l-inner3 u-pb50px u-mb100px u-pb0em--sp u-mb6em--sp is-hide1">
+          <div class="c-box1--b">
+            <h3 class="c-ttl5">
+              <span class="js--eff1">RESULTS</span>
+            </h3>
+            <p class="c-ttl6 u-mb2em">
+              <span class="js-eff1">実績</span>
+            </P>
+            <ul class="u-grid u-gutter40px u-gutter0px--sp u-pb4em--sp">
+              <li class="u-col4 u-col12--sp">
+                <a class="c-card4" href="https://www.jenerate.co.jp/achievement/%e6%96%b0%e5%8e%9f%e6%96%99%e3%83%bb%e7%b4%a0%e6%9d%90%e9%a0%98%e5%9f%9f%e3%81%ab%e3%81%8a%e3%81%91%e3%82%8b%e6%96%b0%e8%a6%8f%e4%ba%8b%e6%a5%ad%e7%ab%8b%e4%b8%8a%e6%94%af%e6%8f%b4pj/">
+                  <div class="c-card4__header">
+                    <p class="c-card4__cat">
+                    <span class="js-eff1 js-eff1--on">【東証一部上場】 住宅関連商材、配管関連商材等を扱う大手化学メーカー</span>
+                    </P>
+                  </div>
+                  <div class="c-card4__ttl">
+                    新技術を活用した新規事業企画、実行支援プロジェクト
+                  </div>
+                </a>
+              </li>
+              <li class="u-col4 u-col12--sp">
+                <a class="c-card4" href="https://www.jenerate.co.jp/achievement/562/">
+                  <div class="c-card4__header">
+                    <p class="c-card4__cat">
+                    <span class="js-eff1 js-eff1--on">【Fortune 500】米国に本社を置く大手保険ブローカー</span>
+                  </P>
+                  </div>
+                  <div class="c-card4__ttl">
+                    保険流通におけるデジタル活用を軸とした新規事業創出支援プロジェクト
+                  </div>
+                </a>
+              </li>
+              <li class="u-col4 u-col12--sp">
+                <a class="c-card4" href="https://www.jenerate.co.jp/achievement/%e6%96%b0%e3%81%9f%e3%81%aa%e3%80%8c%e5%83%8d%e3%81%8d%e6%96%b9%e3%80%8d%e3%81%ae%e6%bd%ae%e6%b5%81%e3%82%92%e8%a6%8b%e8%b6%8a%e3%81%97%e3%81%9f%e6%96%b0%e8%a6%8f%e4%ba%8b%e6%a5%ad%e6%a1%88%e3%81%ae/">
+                  <div class="c-card4__header">
+                    <p class="c-card4__cat">
+                    <span class="js-eff1 js-eff1--on">【東証一部上場】文具やオフィス家具 などを扱う大手メーカー</span>
+                    </P>
+                  </div>
+                  <div class="c-card4__ttl">
+                    新たな「働き方」の潮流を見越した新規事業案企画、実行支援プロジェクト
+                  </div>
+                </a>
+              </li>
+            </ul>
+            <a class="c-btn1" href="https://www.jenerate.co.jp/achievement/">
+              <span>実績一覧</span>
+            </a>
+          </div>
+        </section>
+        <nav class="c-breadcrumb l-inner2 u-mt2em u-mb2em">
+          <span property="itemListElement" typeof="ListItem">
+            <span property="name">ホーム</span>
+          </span>
+          <span class="bcn-split">
+          </span>
+          <span class="post post-page current-item">
+            事業案内
+          </span>
+        </nav>
+        <section　class="p-ourBlock4">
+          <img class="is-pc"　src="img-4.jpg">
+          <img class="is-sp" src="img-13.jpg">
+        </section>
+        <nav>
+          <ul class="u-grid f-footrNav">
+             <li class="u-col6 u-col12--sp is-hide1">
+                <a class="c-card3--b" href="https://www.jenerate.co.jp/recruit/">
+                  <p class="c-card3__en">
+                    CAREERS
+                  </p>
+                  <p class="c-card3__ttl">
+                    VENTURE WITH US
+                  </p>
+                  <p class="c-card3__jp c-border1">
+                    採用情報
+                  </p>
+                </a>
+             </li>
+             <li class="u-col6 u-col12--sp">
+                <a class="c-card3" href="https://www.jenerate.co.jp/companyprofile/">
+                  <p class="c-card3__en">
+                    COMPANY
+                  </p>
+                  <p class="c-card3__ttl">
+                    <img class="df" src="logo1.png" alt>
+                    <img class="bk" src="logo2.png" alt>
+                  </p>
+                  <p class="c-card3__jp c-border1">
+                    会社情報
+                  </p>
+                </a>
+             </li>
+          </ul>
+        </nav>
+        </main>
+
+        <!-- Footer -->
+        <footer class="f-footer">
+          <div class="l-inner2">
+            <div class="f-main">
+              <div class="f-block1">
+                <a class="f-logo1">
+                  <img src="logo1.png">
+                </a>
+                <p class="f-lead1">
+                  あらゆる手段を駆使して困難の多い0→1に挑みます。
+                </p>
+                <div class="f-block2">
+                  <div>
+                    <p>
+                      東京都渋谷区
+                      <br class="is-sp">
+                        恵比寿南3-1-19恵比寿ライトビル4F
+                    </p>
+                    <p class="f-tel">
+                      TEL:03-4405-6844
+                    </p>
+                  </div>
+                  <a class="c-btn2" href="">
+                    <span>CONTACT</span>
+                  </a>
+                </div>
+              </div>
+              <nav class="f-nav">
+                <div>
+                  <dl>
+                    <dt>
+                      私たちについて
+                    </dt>
+                  </dl>
+                  <dl class="js-btn">
+                    <dt>
+                      事業案内
+                    </dt>
+                    <dd style="display: none;">新規事業コンサルティング</dd>
+                    <dd style="display: none;">ジャパンエントリー</dd>
+                    <dd style="display: none;">イベントベストメント</dd>
+                    <dd style="display: none;">実績</dd>
+                  </dl>
+                  <dl>
+                    <dt>
+                      会社情報
+                    </dt>
+                  </dl>
+                </div>
+                <div>
+                  <dl class="js-btn is-hide1">
+                    <dt>
+                      採用情報
+                    </dt>
+                    <dd style="display: none;">キャリアパス</dd>
+                    <dd style="display: none;">働く環境</dd>
+                    <dd style="display: none;">募集要項</dd>
+                    <dd style="display: none;">メンバーインタビュー</dd>
+                  </dl>
+                  <dl class="js-btn is-hide0805">
+                    <dt>
+                      レポート
+                    </dt>
+                    <dd style="display: none;">事例</dd>
+                    <dd style="display: none;">トレンド</dd>
+                  </dl>
+                </div>
+                <div>
+                  <dl>
+                    <dt>
+                      お問い合わせ
+                    </dt>
+                  </dl>
+                  <dl>
+                    <dt>
+                      プライバシーポリシー
+                    </dt>
+                  </dl>
+                  <dl class="f-btn1">
+                    <dt>
+                      <a class="c-ico1">
+                        JIX CAPITAL
+                      </a>
+                    </dt>
+                  </dl>
+                </div>
+              </nav>
+              <ul class="h-gNav__nav2 is-hide2">
+                <li>
+                  <a class="current" href>JP</a>
+                </li>
+                <li>
+                  <a href>EN</a>
+                </li>
+              </ul>
+            </div>
+            <p class="f-copyright">© 2019 Jenerate Partners Inc.</p>
+          </div>
+        </footer>
+      </div>
+    </body>
+  </html>
+</template>
+
+<script>
+  export default {
+    head(){
+      return {
+        title: "事業案内",
+        meta: [
+          { hid: 'description', name: 'description', content: 'My custom description' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+
+
+  footer {
+    padding: 57px 0 0px;
+    text-align: center;
+    background-color:0B1A28
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,298 @@
+<template>
+  <html>
+    <body id="top">
+      <div class="wrap">
+
+        <!-- Page Header -->
+        <header class="h-header">
+          <h1 class="h-logo">
+            <a href="https://www.jenerate.co.jp">
+              <img class="h-logo__img1" src="logo1.png">
+              <img class="h-logo__img2" src="logo2.png">
+            </a>
+          </h1>
+          <ul class="h-gNav__nav2--en">
+            <li>
+              <a href="https://www.jenerate.co.jp/">JP</a>
+            </li>
+            <li>
+              <a class="current">EN</a>
+            </li>
+          </ul>
+          <nav class="h-gNav">
+            <ul class="h-gNav__nav1">
+              <li id="menu-item-50" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-11 current_page_item menu-item-50">
+                <router-link to="">私たちについて</router-link>
+              </li>
+              <li id="menu-item-47" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47">
+                <router-link to="/business">事業案内</router-link>
+              </li>
+              <li id="menu-item-48" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-48">
+                <a href="https://www.jenerate.co.jp/companyprofile/">会社情報</a>
+              </li>
+              <li id="menu-item-526" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-526">
+                <a href="https://www.jenerate.co.jp/recruit/">採用情報</a>
+              </li>
+              <li id="menu-item-527" class="is-hide0805 menu-item menu-item-type-post_type_archive menu-item-object-report menu-item-527">
+                <a href="https://www.jenerate.co.jp/report/">レポート</a>
+              </li>
+            </ul>
+            <ul class="h-gNav__nav2">
+              <li>
+                <a href="https://www.jenerate.co.jp/">JP</a>
+              </li>
+              <li>
+                <a href="https://www.jenerate.co.jp/english">EN</a>
+              </li>
+            </ul>
+            <a class="c-btn4" href="https://www.jenerate.co.jp/contactus/">
+              <span>CONTACT</span>
+            </a>
+            <div class="h-menu">
+              <img class="h-menu-bk" src="menu1-bk.png" alt>
+            </div>
+          </nav>
+          <nav class="c-nav4">
+            <div class="c-nav4__inner">
+              <div class="c-nav4__img c-bg1" style="background-image:url('https://www.jenerate.co.jp/content/wp-content/themes/jp/assets/images/top/img3.jpg');"></div>
+              <div class="c-nav4__box">
+                <div>
+                  <div class="c-nav4__logo">
+                    <img class="h-menu-df" src="txt1.png">
+                  </div>
+                  <div class="c-nav4__main">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <aside class="c-mv1">
+          <div class="c-mv1__ttlBlock js-eff1">
+            <h2 class="c-ttl2">
+              <span class="js-eff1">私たちについて</span>
+            </h2>
+            <p class="c-ttl3">
+              <span class="js-eff1">OUR STRENGTHS</span>
+            </p>
+          </div>
+          <div class="c-mv1__imgBlock">
+            <p class="js-eff1">
+              <img src="img-1.jpg">
+            </p>
+          </div>
+        </aside>
+
+        <!-- Main Content -->
+        <main>
+          <section class="l-inner4 p-ourBlock1">
+            <h3 class="c-ttl4-2">
+              Zero to
+              <strong>One</strong>
+            </h3>
+            <div class="c-lead1">
+              <span class="js-eff1">
+                  いま世界は、社会構造の変化や価値観の多様化など、かつてないほどのスピードで変化しています。<br>
+                  社会が抱えるニーズや欲求、社会問題も一層複雑且つ解決が困難なものになっています。<br>
+                  そして、ビジネスの成長と衰退のサイクルも加速度的に進み、従来の考え方やアプローチなど、今あることの延長線上に成長が描きにくい時代に突入しています。<br>
+                  だからこそ、これまでの“当たり前”に疑問を投げかけ、本質的に価値のあること、意味のあることを愚直に追求し、刺激的な未来を創造していくことに意義があります。<br>
+                  それは“0から1”という誰も踏み込んだことのない領域に果敢にチャレンジすることです。<br>
+              </span>
+            </div>
+          </section>
+          <section class="p-ourBlock2 u-mt100px u-pb100px u-mt3em--sp u-pb3em--sp">
+            <p class="p-ourImg1 is-pc">
+              <img src="img-3.jpg">
+            </p>
+            <p class="p-ourImg2 is-sp">
+              <img src="pic-1.jpg">
+            </p>
+            <div class="u-posRelative">
+              <div class="p-ourBlock3 u-sansSerif2">
+                <p class="c-lead1">
+                  Jenerate Partnersは、“Venture for Extraordinary”というスローガンに想いを込めて、特別な未来の実現に向けて、“0から1” に情熱を持った新規事業立ち上げのプロフェッショナルが集い、事業創造に果敢にチャレンジを続けながら「事業創造が活発な社会」の構築を目指しています。
+                </p>
+                <div class="u-mt70px u-mt2em--sp">
+                  <a class="c-btn1 u-mr40px u-mr0px--sp u-mb1_5em" href="">
+                    <span>事業案内</span>
+                  </a>
+                  <a class="c-btn1" href="">
+                    <span>メンバー紹介</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
+          <nav class="c-breadcrumb l-inner2 u-mt2em u-mb2em">
+            <span property="itemListElement" typeof="ListItem">
+              <a property="item" class="home">
+                <span property="name">ホーム</span>
+              </a>
+            </span>
+            <span class="bcn-split">
+            </span>
+            <span class="post post-page current-item">
+              私たちについて
+            </span>
+          </nav>
+          <section　class="p-ourBlock4">
+            <img class="is-pc"　src="img-4.jpg">
+            <img class="is-sp" src="img-13.jpg">
+          </section>
+          <nav>
+            <ul class="u-grid f-footrNav">
+               <li class="u-col6 u-col12--sp is-hide1">
+                  <a class="c-card3--b" href="https://www.jenerate.co.jp/recruit/">
+                    <p class="c-card3__en">
+                      CAREERS
+                    </p>
+                    <p class="c-card3__ttl">
+                      VENTURE WITH US
+                    </p>
+                    <p class="c-card3__jp c-border1">
+                      採用情報
+                    </p>
+                  </a>
+               </li>
+               <li class="u-col6 u-col12--sp">
+                  <a class="c-card3" href="https://www.jenerate.co.jp/companyprofile/">
+                    <p class="c-card3__en">
+                      COMPANY
+                    </p>
+                    <p class="c-card3__ttl">
+                      <img class="df" src="logo1.png" alt>
+                      <img class="ov" src="logo2.png" alt>
+                    </p>
+                    <p class="c-card3__jp c-border1">
+                      会社情報
+                    </p>
+                  </a>
+               </li>
+            </ul>
+          </nav>
+        </main>
+
+
+
+
+      <!-- Footer -->
+        <footer class="f-footer">
+          <div class="l-inner2">
+            <div class="f-main">
+              <div class="f-block1">
+                <a class="f-logo1">
+                  <img src="logo1.png">
+                </a>
+                <p class="f-lead1">
+                  あらゆる手段を駆使して困難の多い0→1に挑みます。
+                </p>
+                <div class="f-block2">
+                  <div>
+                    <p>
+                      東京都渋谷区
+                      <br class="is-sp">
+                        恵比寿南3-1-19恵比寿ライトビル4F
+                    </p>
+                    <p class="f-tel">
+                      TEL:03-4405-6844
+                    </p>
+                  </div>
+                  <a class="c-btn2" href="">
+                    <span>CONTACT</span>
+                  </a>
+                </div>
+              </div>
+              <nav class="f-nav">
+                <div>
+                  <dl>
+                    <dt>
+                      私たちについて
+                    </dt>
+                  </dl>
+                  <dl class="js-btn">
+                    <dt>
+                      事業案内
+                    </dt>
+                    <dd style="display: none;">新規事業コンサルティング</dd>
+                    <dd style="display: none;">ジャパンエントリー</dd>
+                    <dd style="display: none;">イベントベストメント</dd>
+                    <dd style="display: none;">実績</dd>
+                  </dl>
+                  <dl>
+                    <dt>
+                      会社情報
+                    </dt>
+                  </dl>
+                </div>
+                <div>
+                  <dl class="js-btn is-hide1">
+                    <dt>
+                      採用情報
+                    </dt>
+                    <dd style="display: none;">キャリアパス</dd>
+                    <dd style="display: none;">働く環境</dd>
+                    <dd style="display: none;">募集要項</dd>
+                    <dd style="display: none;">メンバーインタビュー</dd>
+                  </dl>
+                  <dl class="js-btn is-hide0805">
+                    <dt>
+                      レポート
+                    </dt>
+                    <dd style="display: none;">事例</dd>
+                    <dd style="display: none;">トレンド</dd>
+                  </dl>
+                </div>
+                <div>
+                  <dl>
+                    <dt>
+                      お問い合わせ
+                    </dt>
+                  </dl>
+                  <dl>
+                    <dt>
+                      プライバシーポリシー
+                    </dt>
+                  </dl>
+                  <dl class="f-btn1">
+                    <dt>
+                      <a class="c-ico1">
+                        JIX CAPITAL
+                      </a>
+                    </dt>
+                  </dl>
+                </div>
+              </nav>
+              <ul class="h-gNav__nav2 is-hide2">
+                <li>
+                  <a class="current" href>JP</a>
+                </li>
+                <li>
+                  <a href>EN</a>
+                </li>
+              </ul>
+            </div>
+            <p class="f-copyright">© 2019 Jenerate Partners Inc.</p>
+          </div>
+        </footer>
+      </div>
+    </body>
+  </html>
+</template>
+
+<script>
+  export default {
+    head(){
+      return {
+        title: "私たちについて",
+        meta: [
+          { hid: 'description', name: 'description', content: 'My custom description' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
